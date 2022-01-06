af1f3da3ee2741786ae81d9718614f91b47e4823
lhp-001 <69450654+lhp-001@users.noreply.github.com>
2022-01-06 16:36:42 +0800

    Delete gkb001-be directory

--- gkb001-be/dev-gkb001-app/src/main/java/com/yonyou/ucf/mdf/app/controller/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * TODO description
- *
- * @author leon
- * @date 2020/4/29
- * @since UCF1.0
- */
-package com.yonyou.ucf.mdf.app.controller;
--- gkb001-be/dev-gkb001-domain/src/main/java/com/yonyou/ucf/mdf/domain/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * TODO description
- *
- * @author leon
- * @date 2020/4/22
- * @since UCF1.0
- */
-package com.yonyou.ucf.mdf.domain;
